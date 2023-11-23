456a9f62b96829876059023b52d060a503fd79f1
Thamizharasu Soundrapandian <tham@softwareag.com>
2023-11-23 22:25:25 +0530

    responsive work

--- src/views/Responsive.vue
+++ src/views/Responsive.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="text-red-900 sm:text-green-900 md:text-blue-900">
-    Responsive
+  <div>
+    <div class="text-red-900 sm:text-green-900 md:text-blue-900 font-bold">
+      Responsive
+    </div>
+
+    <div class="border border-red-500 max-[300px]:border-blue-500">
+      Hello Tham!
+    </div>
   </div>
 </template>
